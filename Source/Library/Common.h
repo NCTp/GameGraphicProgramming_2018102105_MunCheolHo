aca333af4afd60dff0cf0ef678c122ce05d25bc6
NCTP <cjfgh530@naver.com>
2022-03-20 08:37:40 +0900

    [LAB 01] first commit

--- Source/Library/Common.h
+++ Source/Library/Common.h
@@ -16,3 +16,12 @@
 #endif // ! UNICODE
 
 #include <windows.h>
+
+#include <d3d11_4.h>
+#include <directxcolors.h>
+
+#include "Resource.h"
+
+constexpr LPCWSTR PSZ_COURSE_TITLE = L"Game Graphics Programming";
+
+using namespace DirectX;
